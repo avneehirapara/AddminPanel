60801c0c000ad366856432b785994447414ab9dd
avaneehirapara <avnihirapara0706@gmail.com>
2022-09-23 09:35:11 +0530

    23_09_22

--- AddminPanel/redux/action/Catagoris.action.js
+++ AddminPanel/redux/action/Catagoris.action.js
@@ -62,7 +62,7 @@ export const updateCategory = (data) => (dispatch) => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify   (data),
         })
         .then((response) => response.json())
         .then((data) => dispatch({ type: ActionType.UPDATE_CATEGORY, payload: data }))
